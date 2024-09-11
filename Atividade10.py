6b82e05b5785255f71231785fc37419009f55218
Luziferus666 <vieirafontinelebru@gmail.com>
2024-09-11 14:01:50 -0300

    Atividade10

---
+++ Atividade10.py
@@ -0,0 +1,6 @@
+# Crie um programa que receba um número inteiro e informe se ele é par ou ímpar.
+numero = int(input("diga seu numero"))
+if numero % 2 == 0:
+    print("o numero é par")
+else:
+    print("o numero é ímpar")
